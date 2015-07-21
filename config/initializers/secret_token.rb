6b4c575832453d796e07473b1713aaf27b58a33b
Anna Natorilla <annagaiel@gmail.com>
2015-07-21 02:18:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Versuet::Application.config.secret_key_base = '43bbd96659aa100225427e61caf6c5d742c71fc9d3faab0ab9c33683be93e0c95333a4437275b2eb49ac9647c1962daa5d8c1b59094dd79c1a94fa03bd518fa0'
