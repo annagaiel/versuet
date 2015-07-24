a13ae68e80d8602106e2b8cf0bb987c2ef8f741c
Anna Natorilla <annagaiel@gmail.com>
2015-07-24 11:29:18 +0000

    Add about page and enter text and profile img

--- config/routes.rb
+++ config/routes.rb
@@ -54,8 +54,9 @@ Versuet::Application.routes.draw do
   #     resources :products
   #   end
   root 'quotes#index'
-
   resources :quotes
-  # The priority is basef upon order of creation: first created -> highest priority.
+  get 'about', :to => 'quotes#about'
+
+  # The priority is based upon order of creation: first created -> highest priority.
 
 end
