91f65ae6c3d6876df19d13bd9f09afa8bd02b61d
Anna Natorilla <annagaiel@gmail.com>
2015-08-12 11:23:54 +0000

    Add model test of unique tag

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  test "unique" do
+  	quote = Quote.create(:author => 'Anna Gaiel', :saying => 'Think possibilities')
+  	#puts quote.inspect
+  	expected = 'AG#' + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
+
   end
+
 end
